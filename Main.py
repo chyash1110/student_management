a179de830ac73a17d60a1b75049e99cf5a7805a2
Yash Chauhan <118417410+chyash1110@users.noreply.github.com>
2023-07-20 14:44:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import csv, os, datetime, time
+import csv, os, datetime, time, stdiomask
 from termcolor import colored
 from prettytable import from_csv, PrettyTable
 
@@ -52,7 +52,7 @@ choice_two = fontcolor.white("Search Student")
 choice_three = fontcolor.white("Update Student")
 choice_four = fontcolor.white("Delete Student")
 choice_five = fontcolor.white("Project Development Team")
-choice_six = fontcolor.white("Quit")
+choice_six = fontcolor.white("Exit")
 
 #choice menu 
 def display_menu():
@@ -84,69 +84,94 @@ def continue_msg():
 # main-function-1
 #function to add students in database
 def add_student():
+	clr_scr()
 	print("\n")
 	border_msg(" Add a New Student's Information To Database ")
 	print("\n")
-	stu_name = input(smb.DONE + fontcolor.green("Student's Name : "))
-	stu_father = input(smb.DONE + fontcolor.green("Father's Name : "))
-	stu_sem = input(smb.DONE + fontcolor.green("Semester (1-8) : "))
-	roll_num = input(smb.DONE + fontcolor.green("Roll No : "))
-	stu_dob = input(smb.DONE + fontcolor.green("DOB (DD/MM/YYYY) : "))
-	stu_sex = input(smb.DONE + fontcolor.green("Sex (M/F/T) : "))
-	phone_num = input(smb.DONE + fontcolor.green("Contact No. (+91) : "))
-	marks1 = input(smb.DONE + fontcolor.green("CO marks : "))
-	marks2 = input(smb.DONE + fontcolor.green("DAA marks : "))
-	marks3 = input(smb.DONE + fontcolor.green("AUTOMATA marks : "))
-	marks4 = input(smb.DONE + fontcolor.green("MICROPROCESSOR marks : "))
-	marks5 = input(smb.DONE + fontcolor.green("JAVA marks : "))
-	student_data = []
-	student_data.append(stu_name)
-	student_data.append(stu_father)
-	student_data.append(stu_sem)
-	student_data.append(roll_num)
-	student_data.append(stu_dob)
-	student_data.append(stu_sex)
-	student_data.append(phone_num)
-	student_data.append(marks1)
-	student_data.append(marks2)
-	student_data.append(marks3)
-	student_data.append(marks4)
-	student_data.append(marks5)
-	header = ['Student','Father','Semester','Roll No','DOB','Sex(M/F/T)','Contact','CO','DAA','AUTOMATA','MICROPROCESSOR','JAVA']
-	with open(raw_database, 'a') as f:
-		writer = csv.writer(f)
-		writer.writerow(i for i in header)
-		writer.writerows([student_data])
-		f.close()
-	
-    #removing duplicate headers and writing new file 'students.csv'
-	inFile = open(raw_database, 'r')
-	outFile = open(student_database, 'w')
-	listLines = []
-	for line in inFile:
-		if line in listLines:
-			continue
-		else:
-			outFile.write(line)
-			listLines.append(line)
-	outFile.close()
-	inFile.close()
-	try:
-		x = PrettyTable()
-		x.field_names = header
-		x.add_row([student_data[0],student_data[1],student_data[2],student_data[3],student_data[4],student_data[5],student_data[6],student_data[7],student_data[8]
-			,student_data[9],student_data[10],student_data[11]])
-		print("\n" + smb.DONE + fontcolor.green("Quick Overview :"))
-		print(fontcolor.white(x))
-		print("\n" + smb.DONE + fontcolor.green("Data Saved Successfully !"))
-	except Exception:
-		print("\n" + smb.WARN + fontcolor.red("Something Went Wrong ! Check Your Code !"))
-	finally:
-		continue_msg()
+	paswrd = stdiomask.getpass( smb.ARROW + fontcolor.cyan("Enter password to continue : "))
+	if(paswrd == 'Admin@123') :
+		print("\n")
+		stu_name = input(smb.DONE + fontcolor.green("Student's Name : "))
+		stu_father = input(smb.DONE + fontcolor.green("Father's Name : "))
+		stu_sem = input(smb.DONE + fontcolor.green("Semester (1-8) : "))
+		roll_num = input(smb.DONE + fontcolor.green("Roll No : "))
+		stu_dob = input(smb.DONE + fontcolor.green("DOB (DD/MM/YYYY) : "))
+		stu_sex = input(smb.DONE + fontcolor.green("Sex (M/F/T) : "))
+		phone_num = input(smb.DONE + fontcolor.green("Contact No. (+91) : "))
+		marks1 = input(smb.DONE + fontcolor.green("CO marks : "))
+		marks2 = input(smb.DONE + fontcolor.green("DAA marks : "))
+		marks3 = input(smb.DONE + fontcolor.green("AUTOMATA marks : "))
+		marks4 = input(smb.DONE + fontcolor.green("MICROPROCESSOR marks : "))
+		marks5 = input(smb.DONE + fontcolor.green("JAVA marks : "))
+		student_data = []
+		student_data.append(stu_name)
+		student_data.append(stu_father)
+		student_data.append(stu_sem)
+		student_data.append(roll_num)
+		student_data.append(stu_dob)
+		student_data.append(stu_sex)
+		student_data.append(phone_num)
+		student_data.append(marks1)
+		student_data.append(marks2)
+		student_data.append(marks3)
+		student_data.append(marks4)
+		student_data.append(marks5)
+		avg=(int(marks1)+int(marks2)+int(marks3)+int(marks4)+int(marks5))/5
+		student_data.append(avg)
+		if avg >= 95 :
+			g='A+'
+		elif avg >=  90 and avg<= 94.9 :
+			g='A'
+		elif avg >=  80 and avg<= 89.9 :
+			g='B'
+		elif avg >=  70 and avg<= 79.9 :
+			g='C'
+		elif avg >=  60 and avg<= 69.9 :
+			g='D'
+		elif avg >=  40 and avg<= 59.9 :
+			g='E'
+		elif avg <40 :
+			g='Fail'
+		student_data.append(g)
+		header = ['Student','Father','Semester','Roll No','DOB','Sex(M/F/T)','Contact','CO','DAA','AUTOMATA','MICROPROCESSOR','JAVA','AVERAGE','GRADE']
+		with open(raw_database, 'a') as f:
+			writer = csv.writer(f)
+			writer.writerow(i for i in header)
+			writer.writerows([student_data])
+			f.close()
+		
+	#removing duplicate headers and writing new file 'students.csv'
+		inFile = open(raw_database, 'r')
+		outFile = open(student_database, 'w')
+		listLines = []
+		for line in inFile:
+			if line in listLines:
+				continue
+			else:
+				outFile.write(line)
+				listLines.append(line)
+		outFile.close()
+		inFile.close()
+		try:
+			x = PrettyTable()
+			x.field_names = header
+			x.add_row([student_data[0],student_data[1],student_data[2],student_data[3],student_data[4],student_data[5],student_data[6],student_data[7],student_data[8]
+				,student_data[9],student_data[10],student_data[11],student_data[12],student_data[13]])
+			print("\n" + smb.DONE + fontcolor.green("Quick Overview :"))
+			print(fontcolor.white(x))
+			print("\n" + smb.DONE + fontcolor.green("Data Saved Successfully !"))
+		except Exception:
+			print("\n" + smb.WARN + fontcolor.red("Something Went Wrong ! Check Your Code !"))
+		finally:
+			continue_msg()
+	else:
+		print("\n" + smb.WARN + fontcolor.red("ERROR ! Wrong Password !"))
+		continue_msg()	
 
 # main-function-2
 #function to search student with roll num
 def search_student():
+	clr_scr()
 	print("\n")
 	border_msg(" Search For a Student Inside Database ")
 	roll = input("\n" + smb.DONE + fontcolor.green("Enter Roll No. To Search : "))
@@ -170,6 +195,8 @@ def search_student():
 					print(smb.DONE + fontcolor.green("AUTOMATA : ") + row[9])
 					print(smb.DONE + fontcolor.green("MICROPROCESSOR : ") + row[10])
 					print(smb.DONE + fontcolor.green("JAVA : ") + row[11])
+					print(smb.DONE + fontcolor.green("AVERAGE : ") + row[12])
+					print(smb.DONE + fontcolor.green("GRADE : ") + row[13])
 					break
 		else:
 		    print("\n" + smb.WARN + fontcolor.red("Student Not Found In Our Database !!!"))
@@ -181,99 +208,129 @@ def search_student():
 #main-function-3
 #function to update student data
 def update_student():
-    print("\n")
-    border_msg(" Update Student's Record In Database ")
-    roll_num = input("\n" + smb.DONE + fontcolor.green("Enter Roll No. To Update : "))
-    try:
-    	index_student = None
-    	updated_data = []
-    	fe = open(student_database, "r", encoding="utf-8")
-    	reader = csv.reader(fe)
-    	counter = 0
-    	for row in reader:
-    		if len(row) > 0:
-    			if roll_num == row[3]:
-    				index_student = counter
-    				print("\n" + fontcolor.green('\t----- RECORD FOUND -----') + "\n")
-    				print(smb.DONE + fontcolor.cyan("Student Found At Index =>"), index_student)
-    				print(smb.DONE + fontcolor.cyan("Student's Name =>"), row[0])
-    				student_data = []
-    				print("\n")
-    				new_stu_name = input(smb.DONE + fontcolor.green("Enter Student's New Name : "))
-    				new_stu_father = input(smb.DONE + fontcolor.green("Enter Father's New Name : "))
-    				new_stu_sem = input(smb.DONE + fontcolor.green("Enter New Semester (1-10) : "))
-    				new_roll_num = input(smb.DONE + fontcolor.green("Enter New Roll No : "))
-    				new_stu_dob = input(smb.DONE + fontcolor.green("Enter New DOB (DD/MM/YYYY) : "))
-    				new_stu_sex = input(smb.DONE + fontcolor.green("Enter New Sex (M/F/T) : "))
-    				new_phone_num = input(smb.DONE + fontcolor.green("Enter New Phone No (+91) : "))
-    				new_marks1 = input(smb.DONE + fontcolor.green("CO marks : "))
-    				new_marks2 = input(smb.DONE + fontcolor.green("DAA marks : "))
-    				new_marks3 = input(smb.DONE + fontcolor.green("AUTOMATA marks : "))
-    				new_marks4 = input(smb.DONE + fontcolor.green("MICROPROCESSOR marks : "))
-    				new_marks5 = input(smb.DONE + fontcolor.green("JAVA marks : "))
-    				student_data.append(new_stu_name)
-    				student_data.append(new_stu_father)
-    				student_data.append(new_stu_sem)
-    				student_data.append(new_roll_num)
-    				student_data.append(new_stu_dob)
-    				student_data.append(new_stu_sex)
-    				student_data.append(new_phone_num)
-    				student_data.append(new_marks1)
-    				student_data.append(new_marks2)
-    				student_data.append(new_marks3)
-    				student_data.append(new_marks4)
-    				student_data.append(new_marks5)
-    				updated_data.append(student_data)
-    			else:
-    				updated_data.append(row)
-    			counter += 1
-    except FileNotFoundError:
-    	print("\n" + smb.WARN + fontcolor.red("No Records To Update !"))
+	clr_scr()
+	print("\n")
+	border_msg(" Update Student's Record In Database ")
+	paswrd = stdiomask.getpass("\n" + smb.ARROW + fontcolor.cyan("Enter password to continue : "))
+	if(paswrd == 'Admin@123') :
+		roll_num = input("\n" + smb.DONE + fontcolor.green("Enter Roll No. To Update : "))
+		try:
+			index_student = None
+			updated_data = []
+			fe = open(student_database, "r", encoding="utf-8")
+			reader = csv.reader(fe)
+			counter = 0
+			for row in reader:
+				if len(row) > 0:
+					if roll_num == row[3]:
+						index_student = counter
+						print("\n" + fontcolor.green('\t----- RECORD FOUND -----') + "\n")
+						print(smb.DONE + fontcolor.cyan("Student's Name =>"), row[0])
+						student_data = []
+						print("\n")
+						new_stu_name = input(smb.DONE + fontcolor.green("Enter Student's New Name : "))
+						new_stu_father = input(smb.DONE + fontcolor.green("Enter Father's New Name : "))
+						new_stu_sem = input(smb.DONE + fontcolor.green("Enter New Semester (1-10) : "))
+						new_roll_num = input(smb.DONE + fontcolor.green("Enter New Roll No : "))
+						new_stu_dob = input(smb.DONE + fontcolor.green("Enter New DOB (DD/MM/YYYY) : "))
+						new_stu_sex = input(smb.DONE + fontcolor.green("Enter New Sex (M/F/T) : "))
+						new_phone_num = input(smb.DONE + fontcolor.green("Enter New Phone No (+91) : "))
+						new_marks1 = input(smb.DONE + fontcolor.green("CO marks : "))
+						new_marks2 = input(smb.DONE + fontcolor.green("DAA marks : "))
+						new_marks3 = input(smb.DONE + fontcolor.green("AUTOMATA marks : "))
+						new_marks4 = input(smb.DONE + fontcolor.green("MICROPROCESSOR marks : "))
+						new_marks5 = input(smb.DONE + fontcolor.green("JAVA marks : "))
+						student_data.append(new_stu_name)
+						student_data.append(new_stu_father)
+						student_data.append(new_stu_sem)
+						student_data.append(new_roll_num)
+						student_data.append(new_stu_dob)
+						student_data.append(new_stu_sex)
+						student_data.append(new_phone_num)
+						student_data.append(new_marks1)
+						student_data.append(new_marks2)
+						student_data.append(new_marks3)
+						student_data.append(new_marks4)
+						student_data.append(new_marks5)
+						avg=(int(new_marks1)+int(new_marks2)+int(new_marks3)+int(new_marks4)+int(new_marks5))/5
+						student_data.append(avg)
+						if avg >= 95 :
+							g='A+'
+						elif avg >=  90 and avg<= 94.9 :
+							g='A'
+						elif avg >=  80 and avg<= 89.9 :
+							g='B'
+						elif avg >=  70 and avg<= 79.9 :
+							g='C'
+						elif avg >=  60 and avg<= 69.9 :
+							g='D'
+						elif avg >=  40 and avg<= 59.9 :
+							g='E'
+						elif avg <40 :
+							g='Fail'
+						student_data.append(g)
+						updated_data.append(student_data)
+					else:
+						updated_data.append(row)
+					counter+=1
+		except FileNotFoundError:
+			print("\n" + smb.WARN + fontcolor.red("No Records To Update !"))
 
-#writing update to csv file
-    if index_student is not None:
-        with open(student_database, "w", encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerows(updated_data)
-            print("\n" + smb.DONE + fontcolor.green("Data Updated Successfully ! "))
-            continue_msg()
-    else:
-        print("\n" + smb.WARN + fontcolor.red("Student Not Found In Our Database !!!"))
-        continue_msg()
+		#writing update to csv file
+		if index_student is not None:
+			with open(student_database, "w", encoding="utf-8") as f:
+				writer = csv.writer(f)
+				writer.writerows(updated_data)
+				print("\n" + smb.DONE + fontcolor.green("Data Updated Successfully ! "))
+				continue_msg()
+		else:
+			print("\n" + smb.WARN + fontcolor.red("Student Not Found In Our Database !!!"))
+			continue_msg()
+	else:
+		print("\n" + smb.WARN + fontcolor.red("ERROR ! Wrong Password !"))
+		continue_msg()
         
 # main-function-4
 #function to delete student record
 def delete_student():
-    border_msg(" Delete Student's Record From Database ")
-    roll = input("\n" + smb.WARN + fontcolor.red("Enter Roll No. To Delete : "))
-    try:
-    	student_found = False
-    	updated_data = []
-    	ff = open(student_database, "r", encoding="utf-8")
-    	reader = csv.reader(ff)
-    	counter = 0
-    	for row in reader:
-    		if len(row) > 0:
-    			if roll != row[3]:
-    				updated_data.append(row)
-    				counter += 1
-    			else:
-    				student_found = True
-    except FileNotFoundError:
-    	print("\n" + smb.WARN + fontcolor.red("No Records To Delete !"))
-    if student_found is True:
-        with open(student_database, "w", encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerows(updated_data)
-        print("\n" + smb.DONE + fontcolor.green("Roll No. Deleted Successfully"))
-        continue_msg()
-    else:
-    	print("\n" + smb.WARN + fontcolor.red("Roll No. Not Found In Our Database !!!"))
-    	continue_msg()
+	clr_scr()
+	print("\n")
+	border_msg(" Delete Student's Record From Database ")
+	paswrd = stdiomask.getpass("\n" + smb.ARROW + fontcolor.cyan("Enter password to continue : "))
+	if(paswrd == 'Admin@123') :
+		roll = input("\n" + smb.WARN + fontcolor.red("Enter Roll No. To Delete : "))
+		try:
+			student_found = False
+			updated_data = []
+			ff = open(student_database, "r", encoding="utf-8")
+			reader = csv.reader(ff)
+			counter = 0
+			for row in reader:
+				if len(row) > 0:
+					if roll != row[3]:
+						updated_data.append(row)
+						counter += 1
+					else:
+						student_found = True
+		except FileNotFoundError:
+			print("\n" + smb.WARN + fontcolor.red("No Records To Delete !"))
+		if student_found is True:
+			with open(student_database, "w", encoding="utf-8") as f:
+				writer = csv.writer(f)
+				writer.writerows(updated_data)
+				print("\n" + smb.DONE + fontcolor.green("Roll No. Deleted Successfully"))
+				continue_msg()
+		else:
+			print("\n" + smb.WARN + fontcolor.red("Roll No. Not Found In Our Database !!!"))
+			continue_msg()
+	else :
+		print("\n" + smb.WARN + fontcolor.red("ERROR ! Wrong Password !"))
+		continue_msg()
 
 #main-function-5
-#about us
+#about team
 def about_me():
+	clr_scr()
 	print("\n")
 	border_msg(" Project Development Team ")
 	#print("\n")
@@ -301,10 +358,11 @@ while True:
     elif choice == '5':
     	about_me()
     elif  choice == '6':
+    	clr_scr()
+    	border_msg(" EXITING ")
     	print("\n" + smb.DONE + fontcolor.green("Thanks For Using My Academic Evaluation System"))
-    	print(smb.WARN + fontcolor.red("Quitting...."))
     	time.sleep(2)
     	quit()
     else:
-    	   print("\n" + smb.WARN + fontcolor.red("Please, Enter A Valid Input !"))
+    	   print("\n" + smb.WARN + fontcolor.red("Please, Enter a Valid Input !"))
     	   break
